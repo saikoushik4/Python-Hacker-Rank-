c045981f046deac3d29fc28438a467f4a4ffa8a7
saikoushik4 <165674317+saikoushik4@users.noreply.github.com>
2025-01-22 11:49:41 +0530

    Update input().py

--- input().py
+++ input().py
@@ -1,6 +1,6 @@
 x, _ = map(int, input().split())
 polynomial = input()
-if eval(polynomial.replace('x', str(x))) == 0:
+if not eval(polynomial.replace('x', str(x))) == 0:
     print(True)
 else:
     print(False)
